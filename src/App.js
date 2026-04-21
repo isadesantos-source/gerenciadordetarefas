3e7bef61cd750c44e9c693ba95e4b4ce5a25a4e9
“Seu <email@edu.uniube.br>
2026-04-21 18:57:40 -0300

    Atualiza App, Tarefa e ListaDeTarefas

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { TarefaProvider, useTarefas } from "./tarefa.js";
+import ListaDeTarefas from "./listadetarefa.js";
 
-function App() {
+function AppContent() {
+  const [texto, setTexto] = useState("");
+  const { state, dispatch } = useTarefas();
+
+  function adicionar() {
+    if (!texto.trim()) return;
+
+    dispatch({
+      type: "adicionar",
+      payload: {
+        id: Date.now(),
+        texto,
+        concluida: false,
+      },
+    });
+    setTexto("");
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        padding: 20,
+      }}
+    >
+      <h1>Gerenciador de tarefas</h1>
+      <input
+        value={texto}
+        onChange={(e) => setTexto(e.target.value)}
+        placeholder="Nova tarefa"
+      />
+      <button onClick={adicionar}>adicionar</button>
+
+      <select
+        value={state.filtro}
+        onChange={(e) => dispatch({ type: "filtro", payload: e.target.value })}
+      >
+        <option value="todas">Todas</option>
+        <option value="concluida">Concluídas</option>
+        <option value="pendentes">Pendentes</option>
+      </select>
+
+      <ListaDeTarefas />
     </div>
   );
 }
-
-export default App;
+export default function App() {
+  return (
+    <TarefaProvider>
+      <AppContent />
+    </TarefaProvider>
+  );
+}
